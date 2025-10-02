e32056bfcbb88bbf8fd0a0806e56346398ad67ad
Cryfear <vasil.arturok@yandex.ru>
2025-10-02 10:19:42 +0300

    some functionality

---
+++ src/core/dom.js
@@ -0,0 +1,41 @@
+class Dom {
+  constructor(selector) {
+    this.$el = typeof selector === 'string'
+      ? document.querySelector(selector)
+      : selector;
+  }
+
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html;
+      return this;
+    }
+    return this.$el.outerHTML.trim();
+  }
+
+  clear() {
+    this.html('');
+    return this;
+  }
+
+  append(nodeElement) {
+    if (nodeElement instanceof Dom) nodeElement = nodeElement.$el;
+
+    if (Element.prototype.append) this.$el.append(nodeElement);
+    else {
+      this.$el.appendChild(nodeElement);
+    }
+
+    return this;
+  }
+}
+
+export function $(selector) {
+  return new Dom(selector);
+}
+
+$.create = (tagName, classes = '') => {
+  const el = document.createElement(tagName);
+  if (classes) el.classList.add(classes);
+  return $(el);
+};
