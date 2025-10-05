e128b18bef8239d69e307436f3ec0d1f393e2589
Cryfear <vasil.arturok@yandex.ru>
2025-10-05 15:01:55 +0300

    dynamic render excel

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,8 +1,33 @@
+import { capitalize } from '@core/utils';
+
 export class DomListener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     if (!$root) {
       throw new Error('DomListener requires a root element.');
     }
     this.$root = $root;
+    this.listeners = listeners;
   }
+
+  initDomListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener);
+      if (!this[method]) {
+        throw new Error(`Method ${method} function not implemented in ${this.name} Component`);
+      }
+      this[method] = this[method].bind(this);
+      this.$root.on(listener, this[method]);
+    });
+  }
+
+  removeDomListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener);
+      this.$root.off(listener, this[method]);
+    });
+  }
+}
+
+function getMethodName(methodName) {
+  return 'on' + capitalize(methodName);
 }
